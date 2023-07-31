89f5c7581c72433b577e7f2f6a66ff27eadeac15
JulianGPereira <109522531+JulianGPereira@users.noreply.github.com>
2023-07-31 17:27:15 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,18 +2,13 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'https://unpkg.com/three@0.154.0/examples/jsm/controls/OrbitControls.js'
 import MouseMeshInteraction from 'three_mmi'
-import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
-import {ID,temp,weight,quantity,dimentions,stage1value,stage2value,stage3value,stage4value} from './logic.js'
-import { setValue,stagesResultAlert,result} from './logic.js';
-import { addEntry } from './logic.js';
+import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js';
 
-var rightx=.55
-var mainz=-2
+//import { addEntry } from './logic.js';
+import {stage1Bulb,stage2Bulb,stage3Bulb,stage4Bulb}  from './bulbAnimation.js';
+import { appendAlert,removeAlert } from './appendAlert.js';
 var realboxMesh5
-var productLabel
-console.log(THREE)
 
-console.log("Id is "+ID)
 const canvas = document.querySelector(".webgl");
 
 //set sizes
@@ -21,6 +16,11 @@ const sizes={
   width : canvas.width,
   height : canvas.height
 }
+//set Scene
+
+const scene =new THREE.Scene()
+scene.background = new THREE.Color(0xB1E1FF);
+
 //renderer
 
  const renderer = new THREE.WebGLRenderer(
@@ -42,11 +42,7 @@ let labelRenderer = new CSS2DRenderer();
   labelRenderer.domElement.style.top = "0px";
   document.body.appendChild(labelRenderer.domElement);
 
-//set Scene
 
-const scene =new THREE.Scene()
-scene.background = new THREE.Color(0xB1E1FF);
-export{scene}
 //Camera
 const camera =new THREE.PerspectiveCamera(75, sizes.width/sizes.height,1,1000)
 camera.position.set(-9,4,-3)
@@ -105,40 +101,15 @@ realboxMesh5.name='product'
 realboxMesh5.castShadow = true;
     realboxMesh5.receiveShadow = true;
 scene.add(realboxMesh5)
-//console.log(Object.values(realboxMesh5))
+
 }
 
-//Bulb Animation on top of stages
-
-const box1Geometry = new THREE.BoxGeometry(.1,.1,.1);
-const box1Material=new THREE.MeshStandardMaterial({ color: grey_color });
-
-  const stage1Bulb = new THREE.Mesh(box1Geometry, box1Material);
-  stage1Bulb.name='bulb1'
-  stage1Bulb.position.set(-6.4,4.45,-1.9)
+//StageBulb from bulbAnimation.js
 scene.add(stage1Bulb);
-
-const box2Geometry = new THREE.BoxGeometry(.1,.1,.1);
-const box2Material=new THREE.MeshStandardMaterial({ color: grey_color });
-const stage2Bulb = new THREE.Mesh(box2Geometry, box2Material);
-  stage2Bulb.name='bulb2'
-  stage2Bulb.position.set(-4.7,4.45,-1.9)
 scene.add(stage2Bulb);
-
-const box3Geometry = new THREE.BoxGeometry(.1,.1,.1);
-const box3Material=new THREE.MeshStandardMaterial({ color: grey_color });
-const stage3Bulb = new THREE.Mesh(box3Geometry, box3Material);
-  stage3Bulb.name='bulb3'
-  stage3Bulb.position.set(-3.1,4.45,-1.9)
-scene.add(stage3Bulb)
-
-const box4Geometry = new THREE.BoxGeometry(.1,.1,.1);
-const box4Material=new THREE.MeshStandardMaterial({ color: grey_color });
-const stage4Bulb = new THREE.Mesh(box4Geometry, box4Material);
-  stage4Bulb.name='bulb4'
-  stage4Bulb.position.set(-1.2,4.45,-1.9)
+scene.add(stage3Bulb);
 scene.add(stage4Bulb);
-export{stage1Bulb,stage2Bulb,stage3Bulb,stage4Bulb}
+
 
 //mouse mesh interactions
 
@@ -199,6 +170,8 @@ loader.load('projectNEW.glb', function(glb){
 
 })
 
+export{renderer,realboxMesh5,scene,camera}
+
 {
 
   //adds the green plain with grass texture
@@ -233,147 +206,4 @@ function animateModel(){
 }
 animateModel()
 
-//Product animation during click
-var stage1 = document.getElementById("stage1");
-stage1.addEventListener("click" , ()=> {
-  stage1Bulb.material.color = orange_color;
-  stage1.disabled=true
-  stage2.disabled=false
-    animateBoxStage(-5,stage1Res,stage1value,temp,grey_color,stage1Bulb);
-});
-
-var stage2 = document.getElementById("stage2");
-stage2.addEventListener("click", ()=> {
-  stage2Bulb.material.color = orange_color;
-  stage2.disabled=true
-  stage3.disabled=false
-  animateBoxStage(-3.7,stage2Res,stage2value,weight,grey_color,stage2Bulb)
-  });
-
-var stage3 = document.getElementById("stage3");
-stage3.addEventListener("click", ()=>{
-  stage3Bulb.material.color = orange_color;
-  stage3.disabled=true
-  stage4.disabled=false
-  animateBoxStage(-1.5,stage3Res,stage3value,quantity,grey_color,stage3Bulb)
-});
-var stage4 = document.getElementById("stage4");
-stage4.addEventListener("click", ()=>{
-  stage4Bulb.material.color = orange_color;
-  stage4.disabled=true
-  
-  animateBoxStage(-.5,stage4Res,stage4value,dimentions,brown_color,stage4Bulb) 
-  addEntry()
-});
-export{stage1,stage2,stage3,stage4}
-//function which animates in a straight line
-function animateBoxStage(xPos,Res,Sval,Pval,color,bulb) {
-  function animationStep() {
-      if (realboxMesh5.position.x < xPos) {
-          realboxMesh5.position.x += 0.03;
-          renderer.render(scene, camera);
-          //console.log(realboxMesh5.position.x);
-          requestAnimationFrame(animationStep);
-      } else {
-        if(result==="Pending")
-        {
-          realboxMesh5.position.set(xPos, 3.6, -1.45);
-        } 
-          setValue(Res,Sval,Pval,bulb);
-          console.log(result)
-        if(result==="Pass")
-        {
-          
-          realboxMesh5.material.color=color
-          realboxMesh5.position.set(1, 3.6, mainz++);
-        }
-          if(result==="Fail")
-            {
-              moveRight(5)
-            }
-          renderer.render(scene, camera);   
-      }
-  }
-  animationStep();
-}
-
-//Function to Append popup when mesh is clicked
-
-const appendAlert = () => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<table class="table">
-         <thead>
-            <tr style ="font-size:10px">
-              <th scope="col">ProductID</th>
-              <th scope="col">Temperature</th>
-              <th scope="col">Weight</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">Dimentions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr style ="font-size:10px">
-              <td>${ID}</td>
-              <td>${temp}</td>
-              <td>${weight}</td>
-              <td>${quantity}</td>
-              <td>${dimentions}</td>
-            </tr>
-           <tr style ="font-size:10px">
-                <th scope="col">Product stages</th>
-               <th scope="col">Stage1</th>
-                <th scope="col">Stage2</th>
-                <th scope="col">Stage3</th>
-                <th scope="col">Stage4</th>
-           </tr>
-           <tr style ="font-size:10px">
-               <td></td>
-                <td>${stagesResultAlert[1]}</td>
-                <td>${stagesResultAlert[2]}</td>
-                <td>${stagesResultAlert[3]}</td>
-                <td>${stagesResultAlert[4]}</td>
-           </tr>
-           <tr style ="font-size:10px">
-              <th scope="col" colspan="2">Product Status</th>
-              <td colspan="3">${result}</td>
-           </tr>
-          </tbody>
-        </table>
-  </div>`
-  ].join('')
-  wrapper.className = "label";
-  wrapper.style.marginTop = "-1em";
-  productLabel = new CSS2DObject(wrapper);
-  productLabel.position.set(0,1,1);
-  realboxMesh5.add(productLabel); 
-
-}
-
-function removeAlert()
-{
-  realboxMesh5.remove(productLabel); 
-}
-
 export {createNewProduct}
-
-//Funvtion which moves boxes to failed section
-function moveRight(zPos)
-{
-  realboxMesh5.material.color=grey_color
-  function animationStep() {
-    if (realboxMesh5.position.z < zPos) {
-        realboxMesh5.position.z += 0.03;
-        renderer.render(scene, camera);
-        //console.log(realboxMesh5.position.z);
-        requestAnimationFrame(animationStep);
-    } else {
-        realboxMesh5.position.set(rightx--, 3.6, zPos);
-        
-        //setValue(Res,Sval,Pval,bulb);
-        
-        renderer.render(scene, camera);   
-    }
-}
-animationStep();
-}
